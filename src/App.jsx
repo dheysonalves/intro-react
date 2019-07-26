8a5eb534f15b734e918b5f37a27577f37a3136db
Dheyson Alves <dheyson.lucas@hotmail.com>
2019-07-26 15:39:16 -0300

    Usando props para componente List

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Table from "./Table";
-/*Add import statement here for step 2*/
+import List from "./List"
 
 class App extends React.Component {
   constructor(props) {
@@ -48,8 +48,7 @@ class App extends React.Component {
   render() {
     let tabChoice = <div />;
 
-    /*Uncomment below for step 2*/
-    /*if (this.state.buttonClicked === "assignments") {
+    if (this.state.buttonClicked === "assignments") {
       tabChoice = (
         <List
           placeholder="Add Assignment..."
@@ -58,7 +57,7 @@ class App extends React.Component {
           title="Assignments"
         />
       );
-    }*/
+    }
 
     /* Change below for step 4*/
 
